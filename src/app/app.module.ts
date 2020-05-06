2b0ff56fd66ea390a73cce0cba2d517fd2c8aa45
victorhugoenri8 <victorhugoenriquezco@gmail.com>
2020-05-05 19:06:50 -0500

    partiturasparaguitarron ya como PWA FALTA resolver contenido mixto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
 
+import {ServicioService} from './service/servicio.service';
+
+import { FormsModule } from '@angular/forms';
+
+import { app_routing } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArtistasComponent } from './component/artistas/artistas.component';
+import { CancionesComponent } from './component/canciones/canciones.component';
+import { LeerComponent } from './component/leer/leer.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistasComponent,
+    CancionesComponent,
+    LeerComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    app_routing,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+
   ],
-  providers: [],
+  providers: [
+    ServicioService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
